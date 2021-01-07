3d7da90b535d8469c95f5dadcb0f8629bcbdbd2f
Difa Surya <difas13@gmail.com>
2021-01-07 14:14:41 +0700

    belajar js sampe function

--- myscript.js
+++ myscript.js
@@ -1,1 +1,11 @@
-alert('hello world');
+// var nama;
+// nama = prompt("Nama : ");
+// alert("nama adalah : " + nama);
+
+// var yakin = confirm("yakin ?");
+
+// if (yakin) {
+//     alert('user menekan tombol ok');
+// } else {
+//     alert("user menekan tombol cancel");
+// }
